74895b6677223b137cd9aebf25e0d86dfa32f8c9
Rosa Reyna <113448078+rreynarive@users.noreply.github.com>
2023-02-26 17:37:50 +0100

    Add files via upload

---
+++ Brick.py
@@ -0,0 +1,22 @@
+import pygame
+
+class brick():
+    def __init__(self, pos_x, pos_y, image_ruta):
+        self.__image = pygame.image.load(image_ruta)
+        self.__rect = self.image.get_rect()
+        self.__rect.move_ip(pos_x, pos_y)
+
+    @property
+    def rect(self):
+        return self.__rect
+
+    @property
+    def image(self):
+        return self.__image
+
+    def move(self, posi_x, posi_y):
+        self.__rect.move(posi_x, posi_y)
+
+class irrompible(brick):
+    pass
+fgbhdhsh
